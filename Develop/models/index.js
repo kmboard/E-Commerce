2603b6f8cb618dcdf6c13d0ff650ce013b6ab680
Kija <kmboard1@yahoo.com>
2022-05-18 21:10:28 -0400

    added models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'catergory_id',
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'catergory_id',
+  onDelete: 'SET NULL',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {though: ProductTag});
 
 module.exports = {
   Product,
